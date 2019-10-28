31aecb9bd015fac4430d9b5ab7094b17434b21a1
IncredibleMonkey <42059680+IncredibleMonkey@users.noreply.github.com>
2019-10-28 09:58:34 +0900

    test

--- src/com/beer/dao/list/ListDao.java
+++ src/com/beer/dao/list/ListDao.java
@@ -3,9 +3,8 @@ package com.beer.dao.list;
 import java.util.List;
 
 import com.beer.vo.list.ListVo;
-//test by sj
+// test 
 public interface ListDao {
-	
 	public int deleteList(String l_category, String s_no, String b_no);
 	public int insertList(String l_category, String s_no, String b_no);
 	
